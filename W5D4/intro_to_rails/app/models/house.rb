429edf36321e5121f14438926179e0a9353f9ee6
Elson Li <elson.li@berkeley.edu>
2019-11-14 23:25:11 -0800

    Update W5D4 homework - intro to rails

--- W5D4/intro_to_rails/app/models/house.rb
+++ W5D4/intro_to_rails/app/models/house.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id      :bigint           not null, primary key
+#  address :string           not null
+#
+
 class House < ApplicationRecord
   validates :address, presence: true
 
