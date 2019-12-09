b2dc5dfa3dc76b68d51a71d582083cf2395e9949
Elson Li <elson.li@berkeley.edu>
2019-12-08 23:12:01 -0800

    Finish W9D1 homework - intro to canvas

--- W9D1/intro_to_canvas/practice.js
+++ W9D1/intro_to_canvas/practice.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
+  // Setting Up Your Canvas Element
+  const canvas = document.getElementById("mycanvas");
+  canvas.height = 500;
+  canvas.width = 500;
+  
+  // Draw a Rectangle
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 500, 500);
 
+  // Draw a Circle
+  ctx.beginPath();
+  ctx.arc(75, 65, 20, 0, 2*Math.PI, true);
+  ctx.strokeStyle = "purple";
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = "purple";
+  ctx.fill();
+
+  // Draw Your Favorite Shape
+  ctx.beginPath();
+  ctx.moveTo(75, 25);
+  ctx.quadraticCurveTo(25, 25, 25, 62.5);
+  ctx.quadraticCurveTo(25, 100, 50, 100);
+  ctx.quadraticCurveTo(50, 120, 30, 125);
+  ctx.quadraticCurveTo(60, 120, 65, 100);
+  ctx.quadraticCurveTo(125, 100, 125, 62.5);
+  ctx.quadraticCurveTo(125, 25, 75, 25);
+  ctx.stroke();
 });
